07502920e1e5757111f9dd26503bfd11af90f891
Natalia Episheva <enjyweb@gmail.com>
2019-03-21 13:42:05 +0300

    pw for lesson 6 - task 1, adding log

---
+++ src/server/Log4j_Example.java
@@ -0,0 +1,9 @@
+package server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j_Example {
+
+    private static final Logger log = LogManager.getLogger(Log4j_Example.class);
+}
